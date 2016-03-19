ca53cc33a4eae4a09bcb106c43a337505aefcccb
David Gaynor <david.gaynor@students.olin.edu>
2016-03-19 02:29:39 -0700

    mobile

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,37 @@
 $(function(){
-	var olinDtechConnector = jsPlumb.getInstance();
-	olinDtechConnector.setContainer($(".explanation-section"));
+	if (!window.matchMedia("(max-device-width: 768px)").matches) {
+		var olinDtechConnector = jsPlumb.getInstance();
+		olinDtechConnector.setContainer($(".explanation-section"));
 
-	var dtechLaunchCampConnector = jsPlumb.getInstance();
-	dtechLaunchCampConnector.setContainer($(".explanation-section"));
+		var dtechLaunchCampConnector = jsPlumb.getInstance();
+		dtechLaunchCampConnector.setContainer($(".explanation-section"));
 
-	olinDtechConnector.importDefaults({
-		Connector:[ "Flowchart"],
-		Anchors : [ "Bottom", "Top" ],
-		Endpoint: "Blank",
-		EndpointStyle : { fillStyle: "#567567"  },
-		PaintStyle: { strokeStyle: "#FF9185", lineWidth: 5 }
-		// Anchor : [ 0.5, 0.5, 1, 1 ]
-	});
+		olinDtechConnector.importDefaults({
+			Connector:[ "Flowchart"],
+			Anchors : [ "Bottom", "Top" ],
+			Endpoint: "Blank",
+			EndpointStyle : { fillStyle: "#567567"  },
+			PaintStyle: { strokeStyle: "#FF9185", lineWidth: 5 }
+			// Anchor : [ 0.5, 0.5, 1, 1 ]
+		});
 
-	dtechLaunchCampConnector.importDefaults({
-		Connector:[ "Flowchart"],
-		Anchors : [ "Bottom", "Top" ],
-		Endpoint: "Blank",
-		EndpointStyle : { fillStyle: "#567567"  },
-		PaintStyle: { strokeStyle: "#FF9185", lineWidth: 5 }
-		// Anchor : [ 0.5, 0.5, 1, 1 ]
-	});
+		dtechLaunchCampConnector.importDefaults({
+			Connector:[ "Flowchart"],
+			Anchors : [ "Bottom", "Top" ],
+			Endpoint: "Blank",
+			EndpointStyle : { fillStyle: "#567567"  },
+			PaintStyle: { strokeStyle: "#FF9185", lineWidth: 5 }
+			// Anchor : [ 0.5, 0.5, 1, 1 ]
+		});
 
-	olinDtechConnector.connect({
-		source:"olin-logo-container",
-		target:"dtech-logo-container"
-	});	
+		olinDtechConnector.connect({
+			source:"olin-logo-container",
+			target:"dtech-logo-container"
+		});	
 
-	dtechLaunchCampConnector.connect({
-		source:"dtech-logo-container",
-		target:"launch-camp-logo-container"
-	});	
+		dtechLaunchCampConnector.connect({
+			source:"dtech-logo-container",
+			target:"launch-camp-logo-container"
+		});	
+	}
 })
