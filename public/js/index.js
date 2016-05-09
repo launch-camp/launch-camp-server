e289a0d4510e7c10ad290a16665b39de693679a9
David Gaynor <david.gaynor@students.olin.edu>
2016-05-08 19:57:59 -0700

    added the info session

--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,7 @@ $(function(){
 			EndpointStyle : { fillStyle: "#567567"  },
 			PaintStyle: { strokeStyle: "#FF9185", lineWidth: 5 }
 			// Anchor : [ 0.5, 0.5, 1, 1 ]
-		});
+		});	
 
 		olinDtechConnector.connect({
 			source:"olin-logo-container",
@@ -32,7 +32,7 @@ $(function(){
 		dtechLaunchCampConnector.connect({
 			source:"dtech-logo-container",
 			target:"launch-camp-logo-container"
-		});	
+		});				
 	}
 
 	$(".email-submit").click(function(e) {
@@ -47,4 +47,48 @@ $(function(){
 			fbq && fbq('track', 'Lead');
 		}
 	})	
+
+	$(".email-submit").click(function(e) {
+		var email = $(".email-input").val();
+		if (email) {
+			submitFormSpree({ email_address: email}, function() {				
+				$(".newsletter-thankyou").show()
+				$(".email-input").attr("disabled", true);
+				$(".email-submit").attr("disabled", true);				
+			})
+
+			fbq && fbq('track', 'Lead');
+		}
+	})
+
+	var submitInfoSession = $(".info-session-submit");
+	var infoSessionInput = $(".info-session-email-input");
+	var infoSuccess = $(".info-session-success")
+	submitInfoSession.click(function() {		
+		var email = infoSessionInput.val();
+
+		if (email) {
+			submitInfoSession.html("Submitting...").removeClass("btn-primary").addClass("btn-warning")
+
+			$.ajax({
+				url: "https://script.google.com/macros/s/AKfycbwsyTAgdJ49o48qLj5WCmwfhXbv_bsXarR4lvQ4WnMlXDIzIitl/exec",
+				method: "POST",
+				data: {email: email},
+				success: function() {
+					$(".last-info-session-text").removeClass("last-info-session-text");
+
+					submitInfoSession.html("Success!").removeClass("btn-warning").addClass("btn-success")
+					submitInfoSession.attr("disabled", true);
+					infoSessionInput.attr("disabled", true);
+					infoSuccess.show();
+					infoSuccess.addClass("last-info-session-text");
+					
+					if (olinDtechConnector && dtechLaunchCampConnector) {						
+						olinDtechConnector.repaintEverything();
+						dtechLaunchCampConnector.repaintEverything();
+					}
+				}
+			})
+		}		
+	})
 })
