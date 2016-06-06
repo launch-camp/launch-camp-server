106a38ec03f2312c4ad66b3f384b2b1f517de079
David Gaynor <david.gaynor@students.olin.edu>
2016-06-06 13:58:54 -0700

    added discount code

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,7 @@
 var selectedSession;
 var selectedSessionNumber;
 
-var couponCodes = ["gunn", "dtech", "hickman", "crystal", "gwc", "parab", "launch", "farley", "wang", "hu", "liyan", "sell", "la", "liu", "bc", "saheli", "search", "brianna", "start", "tria", "shetty"];
+var couponCodes = ["gunn", "dtech", "hickman", "crystal", "gwc", "parab", "launch", "farley", "wang", "hu", "liyan", "sell", "la", "liu", "bc", "saheli", "search", "brianna", "start", "tria", "shetty", "dooley"];
 
 $(function() {
 	$(".session-choice").click(function(e) {
