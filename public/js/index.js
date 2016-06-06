a66f149548f8ac0592f0772419c6cbaf82982e28
David Gaynor <david.gaynor@students.olin.edu>
2016-06-05 20:08:26 -0700

    got rid of the info session stuff

--- public/js/index.js
+++ public/js/index.js
@@ -48,49 +48,49 @@ $(function(){
 		}
 	})	
 
-	var submitInfoSession = $(".info-session-submit");	
-	var infoSuccess = $(".info-session-success");
-	var signupForm = $(".info-session-signup");	
+	// var submitInfoSession = $(".info-session-submit");	
+	// var infoSuccess = $(".info-session-success");
+	// var signupForm = $(".info-session-signup");	
 
-	signupForm.validate({		
-		errorPlacement: function(error, element) {
-			console.log("error", element);
-			if (element.attr("name") == "info session") {
-				$(".info-session-error").html(error);
-			}
+	// signupForm.validate({		
+	// 	errorPlacement: function(error, element) {
+	// 		console.log("error", element);
+	// 		if (element.attr("name") == "info session") {
+	// 			$(".info-session-error").html(error);
+	// 		}
 
-			if (element.attr("name") == "email") {
-				$(".email-error").html(error);
-			}			
+	// 		if (element.attr("name") == "email") {
+	// 			$(".email-error").html(error);
+	// 		}			
 
-			repaint()  			
-		},
-		submitHandler: function(form) {			
-			submitInfoSession.html("Submitting...").removeClass("btn-primary").addClass("btn-warning");						
-			submitInfoSession.attr("disabled", true);      				
-      		$.ajax({
-      			url: "https://script.google.com/macros/s/AKfycbwsyTAgdJ49o48qLj5WCmwfhXbv_bsXarR4lvQ4WnMlXDIzIitl/exec",
-      			method: "POST",
-      			data: $(form).serialize(),
-      			success: function() {      				
-      				signupForm.find("input").attr("disabled", true);
-      				$(".last-info-session-text").removeClass("last-info-session-text");
-      				submitInfoSession.html("Success!").removeClass("btn-warning").addClass("btn-success")      				
-      				infoSuccess.show();
-      				infoSuccess.addClass("last-info-session-text");   
+	// 		repaint()  			
+	// 	},
+	// 	submitHandler: function(form) {			
+	// 		submitInfoSession.html("Submitting...").removeClass("btn-primary").addClass("btn-warning");						
+	// 		submitInfoSession.attr("disabled", true);      				
+ //      		$.ajax({
+ //      			url: "https://script.google.com/macros/s/AKfycbwsyTAgdJ49o48qLj5WCmwfhXbv_bsXarR4lvQ4WnMlXDIzIitl/exec",
+ //      			method: "POST",
+ //      			data: $(form).serialize(),
+ //      			success: function() {      				
+ //      				signupForm.find("input").attr("disabled", true);
+ //      				$(".last-info-session-text").removeClass("last-info-session-text");
+ //      				submitInfoSession.html("Success!").removeClass("btn-warning").addClass("btn-success")      				
+ //      				infoSuccess.show();
+ //      				infoSuccess.addClass("last-info-session-text");   
 
-      				repaint()  			
-      			}
-      		})
-      		return false;      		
-    	}
-	});	
+ //      				repaint()  			
+ //      			}
+ //      		})
+ //      		return false;      		
+ //    	}
+	// });	
 
-	signupForm.find("input[type='radio']").click(function() {
-		// wait for 100 ms for the error message to be reomved
-		setTimeout(repaint, 100);
-	});
-	signupForm.find("input[type='email']").keydown(repaint).blur(repaint)
+	// signupForm.find("input[type='radio']").click(function() {
+	// 	// wait for 100 ms for the error message to be reomved
+	// 	setTimeout(repaint, 100);
+	// });
+	// signupForm.find("input[type='email']").keydown(repaint).blur(repaint)
 
 	function repaint() {
 		console.log("repsinging");
